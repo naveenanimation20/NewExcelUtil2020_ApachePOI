dd108fe9a88bec959ed19eacc65c1db0fb240873
Naveen Khunteta <NaveenKhunteta@Naveen-Mac.local>
2019-12-25 14:05:06 +0530

    adding latest excel util - Xls_Reader class with latest dependency and code

---
+++ src/test/java/excel/tests/ExcelUtilTest.java
@@ -0,0 +1,23 @@
+package excel.tests;
+
+import com.excel.lib.util.Xls_Reader;
+
+public class ExcelUtilTest {
+
+	public static void main(String[] args) {
+
+		
+		Xls_Reader reader = new Xls_Reader("/Users/NaveenKhunteta/Documents/workspace/ExcelUtil/"
+				+ "src/main/java/com/excel/lib/util/SampleExcel.xlsx");
+		String data = reader.getCellData("login", "username", 3);
+		System.out.println(reader.getCellData("login", 1, 2));
+		System.out.println(data);
+		
+		reader.setCellData("login", "status", 2, "pass");
+		
+		reader.addSheet("Naveen2");
+	//	reader.removeColumn("login", 2);
+		
+	}
+
+}
